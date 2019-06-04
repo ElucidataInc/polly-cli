24f5aa87af51491aebd3a3e882160384d8917e77
cpj2195 <chanderprabh.jain@elucidata.io>
2019-06-04 18:28:09 +0530

    changes for cd_polly

--- dev/index.js
+++ dev/index.js
@@ -332,7 +332,7 @@ module.exports.createWorkflowRequest = function (token_filename,
     });
 }
 
-module.exports.createRunRequest = function (token_filename, component_id, project_id) {
+module.exports.createRunRequest = function (token_filename, component_id, project_id,extra_Info) {
     if (has_id_token(token_filename)) {
         public_token_header = read_id_token(token_filename);
     }
@@ -344,6 +344,11 @@ module.exports.createRunRequest = function (token_filename, component_id, projec
         "project_id": project_id,
         "name": "Polly™ QuantFit Untitled"
     };
+    if(extra_Info){
+        payload["metadata"]={
+            "additional_info":extra_Info
+        }
+    }
     var options = {
         method: 'PUT',
         url: 'https://api.devpolly.elucidata.io/run',
