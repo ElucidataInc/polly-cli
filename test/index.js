27f56a4a6f418a2db70a2db9b1619e0140987ae3
cpj2195 <chanderprabh.jain@elucidata.io>
2019-06-24 18:31:20 +0530

    testing import wf

--- test/index.js
+++ test/index.js
@@ -347,7 +347,8 @@ module.exports.createRunRequest = function (token_filename, component_id, projec
     };
     if(extra_Info){
         payload["metadata"]={
-            "additional_info":extra_Info
+            "additional_info":extra_Info,
+            "import_workflow_id":"1-chanderprabh.jain@elucidata.io-1552461097169"
         }
     }
     console.log(payload)
