b47cdf89787b64a18b035926e9bd46a19e835e03
sakshikukreja14 <sakshi.kukreja@elucidata.io>
2021-02-15 12:03:29 +0530

    Add changed api(s) for downloading model and binar in prod

--- prod/index.js
+++ prod/index.js
@@ -979,6 +979,61 @@ module.exports.fetchModels = function (cookie_file) {
     });
 }
 
+module.exports.downloadPeakMLModel = function (model_id, cookie_file) {
+    var urlWithFile = 'https://v2.api.polly.elucidata.io/peakml-models/' + model_id;
+    var options = {
+        method: 'GET',
+        url: urlWithFile,
+        headers:
+            {
+                'content-type': 'application/vnd.api+json',
+                'cookie': cookie_file
+            },
+        json: true,
+    };
+    
+    request(options, function (error, response, body) {
+        if (error) { 
+            throw new Error(chalk.bold.red(error)); 
+        }
+        console.log(chalk.yellow.bgBlack.bold(`PostRun Response: `));
+        if (response.statusCode != 200) {
+            console.error(JSON.stringify(response.body));
+            return;
+        }
+        console.log(chalk.green.bold(JSON.stringify(body)));
+        return body
+    });
+}
+
+module.exports.downloadPeakMLBinary = function (type, os, cookie_file) {
+    var urlWithFile = 'https://v2.api.polly.elucidata.io/peakml-static?type=' + type + '&os=' + os;
+    
+    var options = {
+        method: 'GET',
+        url: urlWithFile,
+        headers:
+            {
+                'content-type': 'application/vnd.api+json',
+                'cookie': cookie_file
+            },
+        json: true,
+    };
+    
+    request(options, function (error, response, body) {
+        if (error) { 
+            throw new Error(chalk.bold.red(error)); 
+        }
+        console.log(chalk.yellow.bgBlack.bold(`PostRun Response: `));
+        if (response.statusCode != 200) {
+            console.error(JSON.stringify(response.body));
+            return;
+        }
+        console.log(chalk.green.bold(JSON.stringify(body)));
+        return body
+    });
+}
+
 require('make-runnable/custom')({
     printOutputFrame: false
 })
